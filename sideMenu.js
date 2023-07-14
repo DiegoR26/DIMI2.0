0842d8210d5c3d83df78e93fded83bfb1537a833
DiegoVoltz2 <diegoerocha26@gmail.com>
2023-07-14 18:00:52 -0300

    Client Data Menu

--- sideMenu.js
+++ sideMenu.js
@@ -3,6 +3,14 @@ const nfsButton = document.getElementById('nfsBt');
 const configButton = document.getElementById('configBt');
 const contentBox = document.getElementById('contentBox');
 
+const clientAddBt = document.getElementById('addBt');
+const clientEditBt = document.getElementById('editBt');
+const clientDelBt = document.getElementById('delBt');
+
+const clientDT = document.getElementById('clientData');
+const clientDTSaveBt = document.getElementById('clientSaveBt');
+const clientDTCancelBt = document.getElementById('clientCancelBt');
+
 clientsButton.addEventListener('click', () => {
     contentBox.style.marginTop = '0';
 });
@@ -13,4 +21,20 @@ nfsButton.addEventListener('click', () => {
 
 configButton.addEventListener('click', () => {
     contentBox.style.marginTop = '-800px';
+});
+
+clientAddBt.addEventListener('click', () => {
+    clientDT.style.marginLeft = '-300px';
+});
+
+clientEditBt.addEventListener('click', () => {
+    clientDT.style.marginLeft = '-300px';
+});
+
+clientDTSaveBt.addEventListener('click', () => {
+    clientDT.style.marginLeft = '0px';
+});
+
+clientDTCancelBt.addEventListener('click', () => {
+    clientDT.style.marginLeft = '0px';
 });
